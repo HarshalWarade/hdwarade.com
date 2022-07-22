574229a8b1d6115cfc40c6f490401e0ead4497a9
HarshalWarade <92624545+HarshalWarade@users.noreply.github.com>
2022-07-22 23:41:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ for (let i = 0; i < toggles.length; i++) {
     toggles[i].addEventListener('click', () => {
         if(parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight){
             contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
-            toggles[i].style.color = "#ff0d52";
+            toggles[i].style.color = "#4385ff";
             icons[i].classList.remove('fa-plus');
             icons[i].classList.add('fa-minus');
         }
